5ac0ce70df12c52187d6a41aeebf2606d9e052e2
Duyen Bui <anhduyen1510@gmail.com>
2017-09-12 09:37:54 +0700

    config database admins table follow devise

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,21 @@
 class Admin < ApplicationRecord
-  belongs_to :manager
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable, :validatable
+  # :registerable, :rememberable, :trackable,
+
+  # ================Association=====================
+
+  # setup self_join for table admin.
+  # we can retieve @admin.partner and @admin.manager
+  has_many :partner, class_name: 'Admin', foreign_key: 'manager_id'
+  belongs_to :manager, class_name: 'Admin', optional: true
+
+  has_many :shops
+
+  # ================ENUMS=====================
+
+  enum role: %w[admin manager partner]
+  enum gender: %w[male female other]
+
 end
--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -1,3 +1,8 @@
 class DeliveryOrder < ApplicationRecord
   belongs_to :user
+  has_many :products_delivery_orders
+  has_many :products, through: :products_delivery_orders
+
+  # ===================Enum==========================
+  enum status: %w[ordered delivered completed]
 end
